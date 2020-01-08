6967faefd0a4890f2579b50d82c03d38e53aa701
Richard Cho <richardcho100@gmail.com>
2020-01-08 17:20:56 -0500

    Set up associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    has_many :grabs
+    has_many :users, through: :grabs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :grabs
+    has_many :books, through: :grabs
 end
