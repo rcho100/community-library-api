052434bd6596af561663b1e7490297b898a38157
Richard Cho <richardcho100@gmail.com>
2020-01-08 16:49:49 -0500

    Generate rails resource for Book & run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_195610) do
+ActiveRecord::Schema.define(version: 2020_01_08_214752) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.boolean "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
