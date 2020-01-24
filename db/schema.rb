6ac7291dabd7eb02093c988be9b76315a9aa1298
Richard Cho <richardcho100@gmail.com>
2020-01-24 14:23:04 -0500

    Remove genre column from books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_225814) do
+ActiveRecord::Schema.define(version: 2020_01_24_192144) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_01_08_225814) do
     t.boolean "available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "genre"
   end
 
   create_table "grabs", force: :cascade do |t|
