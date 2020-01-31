4152cfa43fd719c6401cb5f271754b11ae3a00d9
Richard Cho <richardcho100@gmail.com>
2020-01-30 19:40:47 -0500

    Add error messaging for index action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
     def index
         books = Book.all
-        render json: books, status: 200
+        if books
+            render json: books, status: 200
+        else
+            render json: {message: "Books not found"}
+        end
     end
 
     
