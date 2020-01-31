8d3de0e8708367d6137cabb248b7e6fbb7f9633e
Richard Cho <richardcho100@gmail.com>
2020-01-30 21:53:58 -0500

    Instantiate BooksSerializer in index action in BooksController w/options

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,12 @@ class BooksController < ApplicationController
     def index
         books = Book.all
         if books
-            render json: books, status: 200
+            options = {
+                include: [:users, :grabs]
+            }
+            render json: BookSerializer.new(books, options), status: :ok
         else
-            render json: {message: "Books not found"}
+            render json: {message: "Books not found"}, status: :not_found
         end
     end
 
