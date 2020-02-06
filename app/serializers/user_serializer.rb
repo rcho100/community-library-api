75c0b1db9a559788600a8f4e502692477301ddf6
Richard Cho <richardcho100@gmail.com>
2020-02-05 21:55:29 -0500

    Remove password attribute in UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :email, :password
+  attributes :name, :email
   has_many :grabs
   has_many :books, through: :grabs
 end
