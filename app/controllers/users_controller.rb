922c0ae92f62feeacc7624312c6d39f188e6795d
Richard Cho <richardcho100@gmail.com>
2020-02-05 21:57:29 -0500

    Fix typo/syntax error from user.save? to user.save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
     def create
         user = User.create(user_params)
-        if user.save?
+        if user.save
             render json: { user: UserSerializer.new(user) }, status: :created
         else
             render json: { error: "failed to create user" }, status: :not_acceptable
