345d3c965d7f9d143253630135f37a741743867f
Richard Cho <richardcho100@gmail.com>
2020-02-06 17:06:04 -0500

    Skip authorized for signup & issue token when user signs up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+    
     def index
         users = User.all
         if users
@@ -14,7 +16,8 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.save
-            render json: { user: UserSerializer.new(user) }, status: :created
+            token = encode_token(user_id: user.id)
+            render json: { user: UserSerializer.new(user), jwt: token }, status: :created
         else
             render json: { error: "failed to create user" }, status: :not_acceptable
         end
