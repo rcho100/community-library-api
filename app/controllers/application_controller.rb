dcaf14018989b3bbca9a303ba9db2d4d0ca83d0b
Richard Cho <richardcho100@gmail.com>
2020-02-06 16:53:57 -0500

    Add method for current user & logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,15 @@ class ApplicationController < ActionController::API
             end
         end
     end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            user = User.find_by_id(user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
