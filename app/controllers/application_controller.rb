d931b99c9890e985d90fa414fde768556f7dd75d
Richard Cho <richardcho100@gmail.com>
2020-02-06 16:51:14 -0500

    Add methods to encode & decode tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode(payload, 'mysecret')#'mysecret is temp secret'
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'mysecret')#'mysecret is temp secret'
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
 end
