d3eb2664a0c9bc63aa63bcb792275a00f1d7b2a0
Richard Cho <richardcho100@gmail.com>
2020-02-10 19:23:08 -0500

    Add custom routes for login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post '/signup', to: 'users#create'
+  post 'login', to: 'auth#create'
+  delete 'logout', to: 'auth#create'
 
   resources :grabs
   resources :books
