04c70321db93f1202f02daa18e4f9b0f7246a2fa
Richard Cho <richardcho100@gmail.com>
2020-02-17 16:04:42 -0500

    Add borrow action & private book_params to update availability on borrowed book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,5 +13,22 @@ class BooksController < ApplicationController
         end
     end
 
-    
+    def borrow
+        book = Book.find_by_id(book_params[:id])
+        if book
+            book.available = !book.available
+            options = {
+                include: [:users, :grabs]
+            }
+            render json: BookSerializer.new(book, options), status: :ok
+        else
+            render json: {message: "Book not found"}, status: :not_found
+        end
+
+    end
+
+    private
+    def book_params
+        params.require(:book).permit(:id)
+    end
 end
