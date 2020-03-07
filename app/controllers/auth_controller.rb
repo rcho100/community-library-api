0c193031b597593f9440120de322d8a07eb381b2
Richard Cho <richardcho100@gmail.com>
2020-03-06 22:54:09 -0500

    Delete only condition for skip_before_action: authorized in AuthController

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-    skip_before_action :authorized, only: [:create]
+    skip_before_action :authorized
 
     def create
         user = User.find_by(email: user_login_params[:email])
